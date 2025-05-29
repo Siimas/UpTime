cef6402708d9fd6e66ae76677ffa75eeb26a948b
Siimas <simaomarques20@gmail.com>
2025-05-29 20:20:23 +0100

    Add dashboard components and utilities: implement dashboard page, utility functions for class names, and various UI components including skeleton, label, input, avatar, and site header.

---
+++ frontend/app/(dashboard)/dashboard/monitor/[id]/page.tsx
@@ -0,0 +1,36 @@
+import { ChartAreaInteractive } from "@/components/chart-area-interactive";
+import { DataTable } from "@/components/data-table";
+import { SectionCards } from "@/components/section-cards";
+import { SiteHeader } from "@/components/site-header";
+
+import data from "../../data.json";
+
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params;
+
+  return (
+    <>
+      <SiteHeader title={`Monitor: ${id}`} />
+      <div className="flex flex-1 flex-col">
+        <div className="@container/main flex flex-1 flex-col gap-2">
+          <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+            {/* Basic Stats */}
+            <SectionCards />
+
+            {/* (Live) Monitor Results Chart */}
+            <div className="px-4 lg:px-6">
+              <ChartAreaInteractive />
+            </div>
+
+            {/* (Live) Monitor Results */}
+            <DataTable data={data} />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
